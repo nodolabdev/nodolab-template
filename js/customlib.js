4241a2e3cd16d12355fb1ffbc924520880fdb195
Sebastian Paniagua <spaniagua@H12137.ad.hugeinc.com>
2021-10-16 11:40:24 -0500

    sound bug fixed on scorm package

--- js/customlib.js
+++ js/customlib.js
@@ -5,7 +5,8 @@
  */
 (function (app) {
   function onAppLoaded() {
-    const mainTrack = new Audio("../content/sounds/main_track.mp3");
+    const mainTrack = new Audio("content/sounds/main_track.mp3");
+    mainTrack.load();
     /**
      * To handle when an activity has been completed.
      * @param {event} event
@@ -58,7 +59,7 @@
 
     // Register application event handlers.
     $(app).on("jpit:activity:completed", onActivityCompleted);
-    // $(window).one("click", playPauseMainTrack);
+    $(window).one("click", playPauseMainTrack);
     $('[data-global="play-pause"]').click(playPauseMainTrack);
   }
 
