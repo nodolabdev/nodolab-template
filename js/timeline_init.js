dab61d09670c57c5521f148ae1365b6c5bc964f0
Leonardo Perez <leperez@google.com>
2024-03-27 23:33:33 -0500

    Adds styles to timeline Updates font family CSS var name for one more generic that is not tied to the actual name of the font family used. Also updates icon family following the same guideline

--- js/timeline_init.js
+++ js/timeline_init.js
@@ -1,1 +1,1 @@
- var timeline = new TL.Timeline('makeup-history-timeline', 'content/timeline/makeup-timeline.json', {});
+ var timeline = new TL.Timeline('makeup-history-timeline', 'content/timeline/makeup-timeline.json', {slide_padding_lr: 80, language: "es", use_bc: true});
