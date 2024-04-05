52e3b70c9e6ebc1e9ed90447f5f5dde22e676c64
Leonardo Perez <leperez@google.com>
2024-04-05 02:17:31 -0500

    Content update for SCORM package

--- js/timeline_init.js
+++ js/timeline_init.js
@@ -1,1 +1,1 @@
- var timeline = new TL.Timeline('makeup-history-timeline', 'content/timeline/makeup-timeline.json', {slide_padding_lr: 80, language: "es", use_bc: true});
+ var timeline = new TL.Timeline('makeup-history-timeline', 'content/timeline/makeup-timeline.json', {slide_padding_lr: 90, language: "es", use_bc: true});
