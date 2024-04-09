b17f9413d2de3f6b549877cbfa34ce98c4933038
Leonardo Perez <leperez@google.com>
2024-04-09 00:00:10 -0500

    update timeline config, remove unneeded imports and update zip file with these changes.

--- js/timeline_init.js
+++ js/timeline_init.js
@@ -1,4 +1,4 @@
-var timeline = new TL.Timeline('e-learning-timeline', 'content/timeline/e-learning-timeline.json', { slide_padding_lr: 80, language: "es", use_bc: true });
+var timeline = new TL.Timeline('e-learning-timeline', 'content/timeline/e-learning-timeline.json', { slide_padding_lr: 80, language: "es", use_bc: true, scale_factor: 0.7, duration: 900, timenav_height_min: 120, timenav_height: 120 });
 
 
 var observer = new MutationObserver(function (mutations) {
